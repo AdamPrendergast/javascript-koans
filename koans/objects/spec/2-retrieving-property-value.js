9af4f8b890d0f1c4188fd6b45488ace4c319e917
adamprendergast <adam.prendergast@codecomputerlove.com>
2014-08-06 08:18:38 +0100

    Started objects

--- koans/objects/spec/2-retrieving-property-value.js
+++ koans/objects/spec/2-retrieving-property-value.js
@@ -11,24 +11,34 @@ describe('Retrieving properties', function () {
 			}
 		};
 	});
+
+
 	it('1 - should know how to use . and [] operators to retrieve property values', function () {
-		expect(samurai.name).toBe(__);
-		expect(samurai['name']).toBe(__);
+		expect(samurai.name).toBe('Myamoto');
+		expect(samurai['name']).toBe('Myamoto');
 	});
+
+
 	it('2 - should know that property names are case-sensitive', function () {
-		expect(samurai.Name).toBe(__);
+		expect(samurai.Name).toBe(undefined);
 	});
+
+
 	it('3 - should know how to use variable property name', function () {
 		var propertyName = 'name';
-		expect(samurai.propertyName).toBe(__);
-		expect(samurai[propertyName]).toBe(__);
+		expect(samurai.propertyName).toBe(undefined);
+		expect(samurai[propertyName]).toBe('Myamoto');
 	});
+
+
 	it('4 - should know default operator ||', function () {
 		samurai.title = '';
-		expect(samurai.address.city || 'unknown city').toBe(__);
-		expect(samurai.address.postcode || 'unknown postcode').toBe(__);
-		expect(samurai.title || 'Mr.').toBe(__);
+		expect(samurai.address.city || 'unknown city').toBe('unknown city');
+		expect(samurai.address.postcode || 'unknown postcode').toBe('18+');
+		expect(samurai.title || 'Mr.').toBe('Mr.');
 	});
+
+
 	it('5 - should know guard operator &&', function () {
 		var samurai2, samurai3 = {
 			address: {
@@ -39,6 +49,8 @@ describe('Retrieving properties', function () {
 		expect(samurai2 && samurai2.address && samurai2.address.street).toBe(__);
 		expect(samurai3 && samurai3.address && samurai3.address.street).toBe(__);
 	});
+
+	
 	it('6 - should know how to combine default and guard operators', function () {
 		var samurai2, samurai3 = {
 			address: {
